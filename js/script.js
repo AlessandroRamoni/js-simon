04e948c1071519834a2860428a4d1050a590b372
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-09-05 17:55:53 +0200

    add 5  random numbers in HTML

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 console.log("Lunedì");
-
+/*
 let firstNumber = 0;
 let secondNumber = 0;
 let thirdNumber = 0;
 let fourthNumber = 0;
 let fifthNumber = 0;
-boxNumber = document.getElementById("box");
+let boxNumber = document.getElementById("box");
+let ticking = document.getElementById("ticking");
 
 window.addEventListener("load", function () {
   firstNumber = getRandomNumber(1, 5);
@@ -15,10 +16,58 @@ window.addEventListener("load", function () {
   fifthNumber = getRandomNumber(1, 5);
 
   boxNumber.innerHTML = `${firstNumber}, ${secondNumber}, ${thirdNumber}, ${fourthNumber}, ${fifthNumber}`;
+
+  let countdown = 10;
+
+  const intervalId = setInterval(function () {
+    if (countdown > 0) {
+      console.log(countdown);
+      ticking.innerHTML = countdown;
+    } else {
+      console.log("prova");
+      clearInterval(intervalId);
+      boxNumber.style.display = "none";
+
+    }
+    countdown--;
+  }, 1000);
 });
+*/
+
+let boxNumber = document.getElementById("box");
+let ticking = document.getElementById("ticking");
+let numbers = [];
+
+window.addEventListener("load", function () {
+  const numbers = [];
+
+  for (i = 0; i < 5; i++) {
+    let randomNumber = getRandomNumber(1, 5);
+    numbers.push(randomNumber);
+    // console.log(randomNumber);
+  }
+  //   console.log(numbers);
+  boxNumber.innerHTML = numbers;
+});
+
+///////////////////// FUNZIONI ///////////////////////////
 
 function getRandomNumber(min, max) {
   let range = max - min + 1;
   let random = Math.floor(Math.random() * range) + min;
   return random;
 }
+/*
+function checkValueInArray(array, value) {
+  // dobbiamo ciclare l'array
+  for (let i = 0; i < array.length; i++) {
+    // controllare che l'elemento iesimo dell'array sia uguale o meno a value
+    if (array[i] === value) {
+      console.log(value + " è presente nell'array");
+      return true;
+    }
+  }
+  console.log(value + " NON è presente nell'array");
+  return false;
+}
+*/
