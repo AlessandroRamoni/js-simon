8478db9b4f7cf633933d1b9eba596aa9ede8f332
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-09-05 15:26:51 +0200

    add 5 random numbers in HTML

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 console.log("Lunedì");
+
+let firstNumber = 0;
+let secondNumber = 0;
+let thirdNumber = 0;
+let fourthNumber = 0;
+let fifthNumber = 0;
+boxNumber = document.getElementById("box");
+
+window.addEventListener("load", function () {
+  firstNumber = getRandomNumber(1, 5);
+  secondNumber = getRandomNumber(1, 5);
+  thirdNumber = getRandomNumber(1, 5);
+  fourthNumber = getRandomNumber(1, 5);
+  fifthNumber = getRandomNumber(1, 5);
+
+  boxNumber.innerHTML = `${firstNumber}, ${secondNumber}, ${thirdNumber}, ${fourthNumber}, ${fifthNumber}`;
+});
+
+function getRandomNumber(min, max) {
+  let range = max - min + 1;
+  let random = Math.floor(Math.random() * range) + min;
+  return random;
+}
