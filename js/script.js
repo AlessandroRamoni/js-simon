2080243d97889fb1ae5b26927c84dc630aa4c885
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-09-06 09:14:36 +0200

    trying a new code for third array

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 console.log("Lunedì");
-/*
+
+let boxNumber = document.getElementById("box");
+let ticking = document.getElementById("ticking");
+let numbers = [];
+let tentativi = [];
+
 let firstNumber = 0;
 let secondNumber = 0;
 let thirdNumber = 0;
 let fourthNumber = 0;
 let fifthNumber = 0;
-let boxNumber = document.getElementById("box");
-let ticking = document.getElementById("ticking");
 
 window.addEventListener("load", function () {
   firstNumber = getRandomNumber(1, 5);
@@ -17,7 +20,16 @@ window.addEventListener("load", function () {
 
   boxNumber.innerHTML = `${firstNumber}, ${secondNumber}, ${thirdNumber}, ${fourthNumber}, ${fifthNumber}`;
 
-  let countdown = 10;
+  numbers.push(
+    firstNumber,
+    secondNumber,
+    thirdNumber,
+    fourthNumber,
+    fifthNumber
+  );
+  console.log(numbers);
+
+  let countdown = 5;
 
   const intervalId = setInterval(function () {
     if (countdown > 0) {
@@ -27,16 +39,17 @@ window.addEventListener("load", function () {
       console.log("prova");
       clearInterval(intervalId);
       boxNumber.style.display = "none";
-
+      ticking.style.display = "none";
     }
     countdown--;
   }, 1000);
 });
-*/
 
+/*
 let boxNumber = document.getElementById("box");
 let ticking = document.getElementById("ticking");
 let numbers = [];
+let tentativi = [];
 
 window.addEventListener("load", function () {
   const numbers = [];
@@ -62,10 +75,25 @@ window.addEventListener("load", function () {
       ticking.style.display = "none";
     }
   }, 1000);
+
+  /////////// SPOSTARE QUESTO BLOCCO IN UNA FUNZIONE FUORI?
+
+  let results = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    let singleNumber = numbers[i];
+    console.log(singleNumber);
+
+    if (!tentativi.includes(singleNumber)) {
+      console.log("Bravo");
+      results.push(singleNumber);
+      console.log(results);
+    } else {
+      console.log("Negativo");
+    }
+  }
 });
-
-let tentativi = [];
-
+*/
 setTimeout(function () {
   for (i = 0; i < 5; i++) {
     let numeriInseriti = parseInt(
@@ -74,22 +102,7 @@ setTimeout(function () {
     tentativi.push(numeriInseriti);
   }
   console.log(tentativi);
-}, 6000);
-
-let results = [];
-
-for (let i = 0; i < numbers.length; i++) {
-  let current = numbers[i];
-  console.log(current);
-
-  if (tentativi.includes(current)) {
-    console.log("Bravo");
-    results.push(current);
-    console.log(results);
-  } else {
-    console.log("Negativo");
-  }
-}
+}, 7000);
 
 ///////////////////// FUNZIONI ///////////////////////////
 
